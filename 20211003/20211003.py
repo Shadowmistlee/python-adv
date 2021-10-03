24277b70fd03c322dc23646fd4d5272f84178380
Shadwmistlee <shadowmistlee@gmail.com>
2021-10-03 11:57:20 +0800

    message

--- 20211003/20211003.py
+++ 20211003/20211003.py
@@ -14,6 +14,7 @@ for key in keys:
 for value in what.values():
     print(value)
 '''
+'''
 data = {
     "Name": "Singular",
     "Score": [
@@ -28,26 +29,63 @@ json_data = json.loads(json_str)
 print(json_str)
 print(json_data)
 print(type(json_str))
+'''
 
 import requests
-api_key = "2f7671995fd280c1b8c10843d66b3f93"
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
-city_name = input("Enter city name : ")
-units = "metric"
-lang = "zh_tw"
-send_url=base_url
-send_url+="appid=" + api_key
-send_url += "&q=" + city_name
-send_url += "&units=" + units
-send_url += "&lang=" + lang
-response = requests.get(send_url)
-info = response.json()
-print("%s\n" % send_url)
-response = requests.get(send_url)
-info = response.json()
-print(type(info))
-print("天氣:" + str(info["weather"][0]["description"]))
-print("溫度:" + str(info["main"]["temp"]))
+from tkinter import *
+def get_weather():
+    api_key = "2f7671995fd280c1b8c10843d66b3f93"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    city_name = input('enter city name')
+    units = "metric"
+    lang = "zh_tw"
+
+    send_url = base_url
+    send_url += "appid=" + api_key
+    send_url += "&q=" + city_name
+    send_url += "&units=" + units
+    send_url += "&lang=" + lang
+
+    # print("%s\n" % send_url)
+    response = requests.get(send_url)
+    info = response.json()
+
+    if "main" in info.keys():
+        temp_info = info["main"]["temp"]
+        weather_info = info["weather"][0]["description"]
+        print("City = " + city_name)
+        put_city.config(text="City = " + city_name)
+        print("Temperature = " + str(temp_info))
+        put_temp.config(text="Temperature = " + str(temp_info))
+        print("Description = " + str(weather_info))
+        put_disc.config(text="Description = " + str(weather_info))
+    else:
+        print(" City Not Found ")
+
+windows = Tk()
+windows.title("My Weather")
+
+put_city = Label(windows, text="city:")
+put_city.pack
+
+put_temp = Label(windows, text="temp:")
+put_temp.pack()
+
+put_disc = Label(windows, text="disc:")
+put_disc.pack()
+
+city_info = Entry(windows)
+city_info.pack()
+
+btn = Button(windows, text='get weather', command=get_weather)
+btn.pack()
+
+windows.mainloop()
+
+
+
+
+
 
 '''
 info = {
